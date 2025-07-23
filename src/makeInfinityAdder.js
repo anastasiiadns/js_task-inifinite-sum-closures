456474cefe478176cc0ef2cdadb14ee90386b475
Anastasiia <nastyadenisenko17@gmail.com>
2025-07-23 17:41:41 +0300

    add task solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,27 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  function adder(value) {
+    if (arguments.length === 0) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    sum += value;
+
+    return adder;
+  }
+
+  adder.clear = () => {
+    sum = 0;
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
